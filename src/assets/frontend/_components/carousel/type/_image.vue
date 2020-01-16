b9ede2806ac8ae737c82aeff10c2bfe7a650c5d5
Wan Shah <was.war@outlook.com>
2020-01-16 17:19:11 +0800

    [#3] Cater for Mobile responsive

--- src/assets/frontend/_components/carousel/type/_image.vue
+++ src/assets/frontend/_components/carousel/type/_image.vue
@@ -1,20 +1,38 @@
 <template lang="pug">
-    .banner-component.image
+    .banner-component.image.desktop(v-if='is641')
         a(:href = 'link', v-if = 'link')
-            img(:src = "image", :alt = 'altText')
-        img(:src = "image", :alt = 'altText', v-else)
+            img(:src = "imgUrl + imgDesktop", :alt = 'altText')
+        img(:src = "imgUrl + imgDesktop", :alt = 'altText', v-else)
+    .banner-component.image.mobile(v-else)
+        a(:href = 'link', v-if = 'link')
+            img(:src = "imgUrl + imgMobile", :alt = 'altText')
+        img(:src = "imgUrl + imgMobile", :alt = 'altText', v-else)
 </template>
 
 <script>
+    import Media from '../../../_shares/media.js'
+
     export default {
+        extends : Media,
+
         props : {
             link : {
                 type : [Boolean, String]
             },
 
-            image : {
+            imgUrl : {
                 type : String,
-                default : '../../../assets/images/banner/default-dek.png'
+                default : '../../../assets/images/banner/'
+            },
+
+            imgDesktop : {
+                type : String,
+                default : 'default-desktop.png'
+            },
+
+            imgMobile : {
+                type : String,
+                default : 'default-mobile.png'
             },
 
             altText : {
