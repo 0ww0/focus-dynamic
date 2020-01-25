fad0dd3bbbbdc70933896edb63dd60a6d0e52ba5
Was War <was.war@outlook.com>
2020-01-25 15:27:33 +0800

    [#8] Add Other layout

--- src/assets/frontend/_pages/celebrate/entry.vue
+++ src/assets/frontend/_pages/celebrate/entry.vue
@@ -1,8 +1,84 @@
 <template lang="pug">
     .wrapper-content
+        .container
+            heading(:text='head', subheader)
+            card(stretch, :xmedia = 'false')
+                cardWrapper(:card = 'isResponsive', v-for = 'data in celebrate', :key = 'data.id')
+                    hoverCard(:hover = 'data')
 </template>
 
 <script>
+    import Media from './../../_shares/media.js'
+    import heading from './../../_components/text/_heading.vue'
+    import card from './../../_components/card/_card.vue'
+    import cardWrapper from './../../_components/card/_wrapper'
+    import hoverCard from './../../_components/card/type/_hover.vue'
+
     export default {
+        extends : Media,
+
+        components : {
+            heading,
+            card,
+            cardWrapper,
+            hoverCard
+        },
+
+        data() {
+            return {
+                head : {
+                    title : 'Celebrate',
+                },
+
+                celebrate : [
+                    {
+                        id: 1,
+                        text : 'Celebrate 1',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'center',
+                    },
+
+                    {
+                        id: 2,
+                        text : 'Celebrate 2',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'center',
+                    },
+
+                    {
+                        id: 3,
+                        text : 'Celebrate 3',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'center',
+                    }
+                ]
+            }
+        },
+
+        computed : {
+            isResponsive () {
+                if(this.is801) {
+                    return 'one-third'
+                } else if (this.is481) {
+                    return 'half'
+                } else {
+                    return 'full'
+                }
+            }
+        },
+
+        created() {
+            this.checkMobile()
+        }
     }
 </script>
+
+<style lang="scss" scoped>
+    .wrapper-content {
+        padding-top: 25px;
+        padding-bottom: 25px;
+    }
+</style>
--- src/assets/frontend/_pages/happening/entry.vue
+++ src/assets/frontend/_pages/happening/entry.vue
@@ -1,8 +1,156 @@
 <template lang="pug">
     .wrapper-content
+        .container
+            heading(:text='head', subheader)
+            .event-holder
+                heading(:text='eventTitle', subheader)
+                card(stretch, :xmedia = 'false')
+                    cardWrapper(:card = 'isResponsive', v-for = 'data in events', :key = 'data.id')
+                        happeningCard(:happening = 'data')
+            .promo-holder
+                heading(:text='promoTitle', subheader)
+                card(stretch, :xmedia = 'false')
+                    cardWrapper(:card = 'isResponsive', v-for = 'data in promos', :key = 'data.id')
+                        happeningCard(:happening = 'data')
 </template>
 
 <script>
+    import Media from './../../_shares/media.js'
+    import heading from './../../_components/text/_heading.vue'
+    import card from './../../_components/card/_card'
+    import cardWrapper from './../../_components/card/_wrapper'
+    import happeningCard from './../../_components/card/type/_happening.vue'
+
     export default {
+        extends : Media,
+        components : {
+            heading,
+            card,
+            cardWrapper,
+            happeningCard
+        },
+
+        data() {
+            return {
+                head : {
+                    title : 'What\'s On',
+                },
+
+                eventTitle : {
+                    title : 'Events'
+                },
+
+                promoTitle : {
+                    title : 'Promo'
+                },
+
+                events : [
+                    {
+                        id : 1,
+                        title : 'Event Name 1',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+
+                    {
+                        id : 2,
+                        title : 'Event Name 2',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+
+                    {
+                        id : 3,
+                        title : 'Event Name 3',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+
+                    {
+                        id : 4,
+                        title : 'Event Name 4',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+                ],
+
+                promos : [
+                    {
+                        id : 1,
+                        title : 'Promo Name 1',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+
+                    {
+                        id : 2,
+                        title : 'Promo Name 2',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+
+                    {
+                        id : 3,
+                        title : 'Promo Name 3',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+
+                    {
+                        id : 4,
+                        title : 'Promo Name 4',
+                        info : 'Event Promotion Info Lorem Ipsum Dolar Sect',
+                        label : 'Label Of Place Promo/Event',
+                        imgPath : '',
+                        imgName : 'default-square.png',
+                        align : 'left',
+                    },
+                ]
+            }
+        },
+
+        computed : {
+            isResponsive () {
+                if(this.is801) {
+                    return 'one-quarter'
+                } else if (this.is481) {
+                    return 'half'
+                } else {
+                    return 'full'
+                }
+            }
+        },
+
+        created() {
+            this.checkMobile()
+        }
     }
 </script>
+
+<style lang="scss" scoped>
+    .wrapper-content {
+        padding-top: 25px;
+        padding-bottom: 25px;
+    }
+</style>
