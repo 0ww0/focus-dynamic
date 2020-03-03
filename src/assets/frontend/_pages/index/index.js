c8630e73b4de9d659905e331dfd975e89294fa76
Was War <was.war@outlook.com>
2020-03-03 09:42:53 +0800

    [#26] Update layout

--- src/assets/frontend/_pages/index/index.js
+++ src/assets/frontend/_pages/index/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueAgile from 'vue-agile'
+import Siema from 'vue2-siema'
 
 window.$ = require('jquery')
 
 import Entry from './entry.vue'
 
 Vue.use(VueAgile)
+Vue.use(Siema)
 
 new Vue({
     el : '.wrapper-content',
