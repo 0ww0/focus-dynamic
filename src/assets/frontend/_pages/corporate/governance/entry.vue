a7e5b774a6abbc01a5af36057dad96601d4bb483
Was War <was.war@outlook.com>
2020-03-11 13:46:04 +0800

    [#28] Update the changes

--- src/assets/frontend/_pages/corporate/governance/entry.vue
+++ src/assets/frontend/_pages/corporate/governance/entry.vue
@@ -1,11 +1,45 @@
 <template lang="pug">
     .wrapper-content
+        banner(:banner = 'banner', :url = 'url')
 </template>
 
 <script>
+    import { URL, API, EntryAPI, Banners } from './api/_api.js'
+    import axios from 'axios'
+    import banner from '../../../_components/banner/_heading.vue'
+
     export default {
+        components : {
+            banner,
+        },
+
+        data() {
+            return {
+                url: URL,
+                banner: {
+                    image: {}
+                },
+            }
+        },
+
+        methods : {
+            fetchAxios() {
+                axios.all([
+                    API.post(EntryAPI, { query : Banners }),
+                ])
+                .then(axios.spread(( BannersResp ) => {
+                    let banner = BannersResp.data.data
+                    this.banner = banner.bgovernancesSingleton
+                    this.banner.image = this.banner.image.path
+                }))
+                .catch(axios.spread(( BannersErr ) => {
+                    console.log(BannersErr)
+                }))
+            }
+        },
+
+        created() {
+            this.fetchAxios()
+        }
     }
 </script>
-
-<style lang="scss" scoped>
-</style>
